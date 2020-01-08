4d657f07dab785429559cd3da5a2615cbe7a683d
adamdotg <adamdotg@gmail.com>
2020-01-08 15:32:14 -0600

    modified arrayList and added hashmap

--- src/exercises/ArrayListExercise.java
+++ src/exercises/ArrayListExercise.java
@@ -6,20 +6,31 @@ import java.util.Arrays;
 
 public class ArrayListExercise {
   public static void main(String[] args){
-    //ArrayList<String> numbs = new ArrayList<>();
-    int[] numbs = {1, 2, 6, 8, 7, 10, 22, 9, 31, 44};
+    ArrayList<Integer> numbs = new ArrayList<Integer>();
+      numbs.add(1);
+      numbs.add(2);
+      numbs.add(6);
+      numbs.add(8);
+      numbs.add(7);
+      numbs.add(10);
+      numbs.add(22);
+      numbs.add(9);
+      numbs.add(31);
+      numbs.add(44);
+
     double sum = 0.0;
-    for (int i=0; i<numbs.length; i++){
-      if (numbs[i]%2==0){
-        sum += numbs[i];
+    //int[] numbs = {1, 2, 6, 8, 7, 10, 22, 9, 31, 44};
+    for (Integer numb : numbs) {
+      if (numb % 2 != 0) {
+        sum += numb;
       }
     }
     System.out.println(sum);
 
     String[] words = {"diver", "roads", "cats", "black", "ancient", "words", "house", "I", "to", "for", "is"};
-    for (int i=0; i<words.length; i++){
-      if (words[i].length()==5){
-        System.out.println(words[i]);
+    for (String word : words) {
+      if (word.length() == 5) {
+        System.out.println(word);
       }
 //      else {
 //        System.out.println("This word is not 5 characters in length: "+words[i]);
---
+++ src/exercises/HashMapPractice.java
@@ -0,0 +1,15 @@
+package exercises;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapPractice {
+  public static void main(String[] arg){
+
+
+
+
+
+
+  }
+}
