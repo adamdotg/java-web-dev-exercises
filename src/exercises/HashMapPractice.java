86162e133dd5a23fd0122329fbb92835be381b10
adamdotg <adamdotg@gmail.com>
2020-01-08 19:26:52 -0600

    final 3 exercise

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 
 public class HashMapPractice {
   public static void main(String[] arg){
+    HashMap<Integer, String> students = new HashMap<>();
+    Scanner input = new Scanner(System.in);
+    String newStudent;
 
+    System.out.println("Enter student info");
 
+    do{
+      System.out.print("Student:");
+      newStudent = input.nextLine();
 
+      if (!newStudent.equals("")){
+        System.out.print("ID:");
+        Integer newID = input.nextInt();
+        students.put(newID, newStudent);
 
+        input.nextLine();
+      }
+    } while(!newStudent.equals(""));
 
+    System.out.println(students);
 
   }
 }
