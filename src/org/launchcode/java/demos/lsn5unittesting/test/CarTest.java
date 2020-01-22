be22b98d985e5c53276e395209dfbca4171986c2
adamdotg <adamdotg@gmail.com>
2020-01-22 13:07:08 -0600

    6.2 assertFalse, assertTrue added

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,11 +1,16 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
-import org.junit.Test;
 import org.launchcode.java.demos.lsn5unittesting.main.Car;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 
-public class CarTest {
+import static org.junit.Assert.*;
 
+public class CarTest {    Car test_car;
+    @Before
+    public void createCarObject(){
+      test_car = new Car ("Toyota", "Prius", 10, 50);
+    }
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
     @Test
     public void emptyTest() {
@@ -15,10 +20,16 @@ public class CarTest {
     //TODO: constructor sets gasTankLevel properly
     @Test
     public void testInitialGasTank() {
-        Car test_car = new Car("Toyota", "Prius", 10, 50);
-        assertEquals(10, test_car.getGasTankLevel(), .001);
+      assertEquals(10, test_car.getGasTankLevel(), .001);
     }
-
+    @Test
+    public void testMakeFalse(){
+      assertFalse(test_car.getMake()=="ford");
+    }
+    @Test
+    public void testMakeTrue(){
+      assertTrue(test_car.getMake()=="Toyota");
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
